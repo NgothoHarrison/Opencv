e7b687aab2a338cfa12eb5b9eb909d754637f4ce
Harrison Hank <codinghank@gmail.com>
2024-01-24 21:01:00 +0300

    change to determine if the file loaded is supported

--- read.py
+++ read.py
@@ -1,9 +1,12 @@
 import cv2 as cv
+import sys
 import rescale_video as rv
 
 # Reading images
 img = cv.imread('images/cat1.webp')
 # img = cv.imread('images/dog3.jpeg')
+if img is None:
+    sys.exit('Could not read the image.')
 
 cv.imshow('Cat Image', img)
 # cv.imshow('Dog Image', img)
@@ -16,5 +19,3 @@ capture = cv.VideoCapture('videos/video4.mp4')
 
 cv.imshow('Video', capture)
 rv.rescaleFrame(capture) # the impoted module to rescale videos
-
-
