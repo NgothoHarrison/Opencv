d000711622571fe8bf337f795053feafdb247637
Harrison Hank <codinghank@gmail.com>
2024-01-26 14:08:44 +0300

    modified:   smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -15,4 +15,10 @@ cv.imshow('Gaussian Blur', gauss)
 M_blur = cv.medianBlur(img, 7)
 cv.imshow('Median Blur', M_blur)
 
+# Bilateral Blur
+bilateral = cv.bilateralFilter(img, 10, 35, 25)
+cv.imshow('Bilateral', bilateral)
+
+
+
 cv.waitKey(0)
