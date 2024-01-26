d53558c151b3ee27340ff0c60f73e37d676cb6a4
Harrison Hank <codinghank@gmail.com>
2024-01-26 14:07:49 +0300

    modified:   smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -4,8 +4,15 @@ img = cv.imread('images/cat1.webp')
 cv.imshow('Original', img)
 
 # Averaging
-average = cv.blur(img, (3,3))
+average = cv.blur(img, (7,7))
 cv.imshow('Average Blur', average)
 
+# Gaussian Blur
+gauss = cv.GaussianBlur(img, (7,7), 0)
+cv.imshow('Gaussian Blur', gauss)
+
+# Median Blur
+M_blur = cv.medianBlur(img, 7)
+cv.imshow('Median Blur', M_blur)
 
 cv.waitKey(0)
