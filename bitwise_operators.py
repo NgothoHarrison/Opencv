cfcba836ea074cf0ffa703bdb3b961fbdc90aa43
Harrison Hank <codinghank@gmail.com>
2024-01-30 00:13:56 +0300

    bitwise and

--- bitwise_operators.py
+++ bitwise_operators.py
@@ -12,5 +12,21 @@ cv.imshow('Rectangle', rectangle)
 circle = cv.circle(blank.copy(), (200,200) , 200, 255 , -1)
 cv.imshow('Circle', circle)
 
+# Bitwise AND --> Intersecting regions
+bitwise_and = cv.bitwise_and(rectangle, circle)
+cv.imshow('Bitwise AND', bitwise_and)
+
+# Bitwise OR --> Non-intersecting and intersecting regions
+bitwise_or = cv.bitwise_or(rectange, circle)
+cv.imshow('Bitwise OR', bitwise_or)
+
+# Bitwise XOR --> Non-intersecting regions
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('BITWISE XOR', bitwise_xor)
+
+# Bitwise NOT // Inverts the binary color
+bitwise_not = cv.bitwise_not(circle)
+cv.imshow('Bitwise NOT', bitwise_not)
+
 
 cv.waitKey(0)
