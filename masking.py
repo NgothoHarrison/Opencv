785a5e47db113be57ed3f5e055f49cc0057c02a6
Harrison Hank <codinghank@gmail.com>
2024-01-30 19:41:33 +0300

    modified:   masking.py

--- masking.py
+++ masking.py
@@ -11,13 +11,16 @@ cv.imshow('Blank Image', blank)
 # masking is used to hide certain parts of the image
 
 # making a circle mask on the blank generated image
-mask = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2 ), 100,255, -1)
+mask = cv.rectangle(blank, (img.shape[1]//2, img.shape[0]//2 ),(img.shape[1]//2 +50, img.shape[0]//2 + 50), 100,255, -1)
 cv.imshow('Mask', mask)
 
 # masking the image loaded to the drawn circle mask
 masked = cv.bitwise_and(img, img, mask=mask)
 cv.imshow('Masked Image', masked)
 
+# rectangle mask 
+rect_mask = cv.rectangle(img.shape[:2], (img.shape[1]//2, img.shape[0]//2 ), (img.shape[1]//2 + 50, img.shape[0]//2 + 50), 255, -1)
+cv.imshow('Rectangle Mask', rect_mask)
 
 
 cv.waitKey(0)
